c0000da7abe5a5d3007a31c55dd8d5c780da65cb
Solee Lee <carolinesoleelee@gmail.com>
2018-09-28 05:21:44 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,7 +18,7 @@ def first_challenge
 contacts.each do |person, data|
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
-        value.each do |flavor|
+        value.delete_if do |flavor|
           flavor == "strawberry"
     end
   end
